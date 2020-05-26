c73899129d67e41ef83e0407b2858c788dc88c3d
Glukhachev Sergey <ka_be@bk.ru>
2020-05-26 20:01:45 +0700

    attempt to solve

--- medium8-1-3.cs
+++ medium8-1-3.cs
@@ -1,25 +1,82 @@
-class Bag
-{
-    public List<Item> Items;
-    public int MaxWeidth; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    public void AddItem(string name, int count)
+namespace Task
+{
+    internal class Program
     {
-        int currentWeidth = Items.Sum(item => item.Count);
-        Item targetItem = Items.FirstOrDefault(item => item.Name == name);
-
-        if (targetItem == null)
-            throw new InvalidOperationException();
-
-        if (currentWeidth + count > MaxWeidth)
-            throw new InvalidOperationException();
-
-        targetItem.Count += count;
+        public static void Main(string[] args)
+        {
+        }
     }
-}
 
-class Item
-{
-    public int Count;
-    public string Name;
+    internal class Bag
+    {
+        public readonly int maxWeight;
+        public IEnumerable<Item> Items => _items;
+
+        private readonly List<Item> _items;
+        private int CurrentWeight => _items.Sum(item => item.Count);
+
+        public Bag(int maxWeight)
+        {
+            if (maxWeight > 0)
+            {
+                this.maxWeight = maxWeight;
+                _items = new List<Item>();
+            }
+            else
+                throw new Exception(nameof(maxWeight) + " must be greater than zero");
+        }
+
+        public bool TryAddItem(string name, int count)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new Exception(nameof(name) + " must not be empty");
+
+            if (count <= 0)
+                throw new Exception(nameof(count) + " must be greater than zero");
+
+            if (CurrentWeight + count > maxWeight)
+                return false;
+            else
+            {
+                var targetItem = _items.FirstOrDefault(item => item.name == name);
+
+                if (targetItem == null)
+                    _items.Add(new Item(name, count));
+                else
+                    targetItem.Grow(count);
+
+                return true;
+            }
+        }
+    }
+
+    internal class Item
+    {
+        public readonly string name;
+        public int Count { get; private set; }
+
+        public Item(string name, int count)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new Exception(nameof(name) + " must not be empty");
+
+            if (count <= 0)
+                throw new Exception(nameof(count) + " must be greater than zero");
+
+            this.name = name;
+            Count = count;
+        }
+
+        public void Grow(int count)
+        {
+            if (count <= 0)
+                throw new Exception(nameof(count) + " must be greater than zero");
+
+            Count += count;
+        }
+    }
 }
